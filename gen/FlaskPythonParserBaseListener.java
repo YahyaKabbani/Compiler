1a2654847e7a80539fc5c99f0336e7d9e005f553
iiCODA <iiyahyakabbani@gmail.com>
2025-12-14 20:23:52 -0800

    missed files

---
+++ gen/FlaskPythonParserBaseListener.java
@@ -0,0 +1,255 @@
+// Generated from D:/Applications/Installed/IntelliJ IDEA Community Edition 2025.2.4/projects/Antlr - Copy/grammarPythonFlask/FlaskPythonParser.g4 by ANTLR 4.13.2
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link FlaskPythonParserListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class FlaskPythonParserBaseListener implements FlaskPythonParserListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(FlaskPythonParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(FlaskPythonParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement(FlaskPythonParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement(FlaskPythonParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterImportStmt(FlaskPythonParser.ImportStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitImportStmt(FlaskPythonParser.ImportStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFromImportStmt(FlaskPythonParser.FromImportStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFromImportStmt(FlaskPythonParser.FromImportStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssignment(FlaskPythonParser.AssignmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssignment(FlaskPythonParser.AssignmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunctionDef(FlaskPythonParser.FunctionDefContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunctionDef(FlaskPythonParser.FunctionDefContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParamList(FlaskPythonParser.ParamListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParamList(FlaskPythonParser.ParamListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDecorator(FlaskPythonParser.DecoratorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDecorator(FlaskPythonParser.DecoratorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIfStmt(FlaskPythonParser.IfStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIfStmt(FlaskPythonParser.IfStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterForStmt(FlaskPythonParser.ForStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitForStmt(FlaskPythonParser.ForStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterReturnStmt(FlaskPythonParser.ReturnStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitReturnStmt(FlaskPythonParser.ReturnStmtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSuite(FlaskPythonParser.SuiteContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSuite(FlaskPythonParser.SuiteContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpr(FlaskPythonParser.ExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpr(FlaskPythonParser.ExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterArgList(FlaskPythonParser.ArgListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitArgList(FlaskPythonParser.ArgListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAtom(FlaskPythonParser.AtomContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAtom(FlaskPythonParser.AtomContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDictLiteral(FlaskPythonParser.DictLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDictLiteral(FlaskPythonParser.DictLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterListLiteral(FlaskPythonParser.ListLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitListLiteral(FlaskPythonParser.ListLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterArgument(FlaskPythonParser.ArgumentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitArgument(FlaskPythonParser.ArgumentContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ gen/FlaskPythonParserListener.java
@@ -0,0 +1,189 @@
+// Generated from D:/Applications/Installed/IntelliJ IDEA Community Edition 2025.2.4/projects/Antlr - Copy/grammarPythonFlask/FlaskPythonParser.g4 by ANTLR 4.13.2
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link FlaskPythonParser}.
+ */
+public interface FlaskPythonParserListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void enterProgram(FlaskPythonParser.ProgramContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void exitProgram(FlaskPythonParser.ProgramContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement(FlaskPythonParser.StatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement(FlaskPythonParser.StatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#importStmt}.
+	 * @param ctx the parse tree
+	 */
+	void enterImportStmt(FlaskPythonParser.ImportStmtContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#importStmt}.
+	 * @param ctx the parse tree
+	 */
+	void exitImportStmt(FlaskPythonParser.ImportStmtContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#fromImportStmt}.
+	 * @param ctx the parse tree
+	 */
+	void enterFromImportStmt(FlaskPythonParser.FromImportStmtContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#fromImportStmt}.
+	 * @param ctx the parse tree
+	 */
+	void exitFromImportStmt(FlaskPythonParser.FromImportStmtContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#assignment}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssignment(FlaskPythonParser.AssignmentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#assignment}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssignment(FlaskPythonParser.AssignmentContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#functionDef}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionDef(FlaskPythonParser.FunctionDefContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#functionDef}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionDef(FlaskPythonParser.FunctionDefContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#paramList}.
+	 * @param ctx the parse tree
+	 */
+	void enterParamList(FlaskPythonParser.ParamListContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#paramList}.
+	 * @param ctx the parse tree
+	 */
+	void exitParamList(FlaskPythonParser.ParamListContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#decorator}.
+	 * @param ctx the parse tree
+	 */
+	void enterDecorator(FlaskPythonParser.DecoratorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#decorator}.
+	 * @param ctx the parse tree
+	 */
+	void exitDecorator(FlaskPythonParser.DecoratorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#ifStmt}.
+	 * @param ctx the parse tree
+	 */
+	void enterIfStmt(FlaskPythonParser.IfStmtContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#ifStmt}.
+	 * @param ctx the parse tree
+	 */
+	void exitIfStmt(FlaskPythonParser.IfStmtContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#forStmt}.
+	 * @param ctx the parse tree
+	 */
+	void enterForStmt(FlaskPythonParser.ForStmtContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#forStmt}.
+	 * @param ctx the parse tree
+	 */
+	void exitForStmt(FlaskPythonParser.ForStmtContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#returnStmt}.
+	 * @param ctx the parse tree
+	 */
+	void enterReturnStmt(FlaskPythonParser.ReturnStmtContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#returnStmt}.
+	 * @param ctx the parse tree
+	 */
+	void exitReturnStmt(FlaskPythonParser.ReturnStmtContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#suite}.
+	 * @param ctx the parse tree
+	 */
+	void enterSuite(FlaskPythonParser.SuiteContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#suite}.
+	 * @param ctx the parse tree
+	 */
+	void exitSuite(FlaskPythonParser.SuiteContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpr(FlaskPythonParser.ExprContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpr(FlaskPythonParser.ExprContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#argList}.
+	 * @param ctx the parse tree
+	 */
+	void enterArgList(FlaskPythonParser.ArgListContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#argList}.
+	 * @param ctx the parse tree
+	 */
+	void exitArgList(FlaskPythonParser.ArgListContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#atom}.
+	 * @param ctx the parse tree
+	 */
+	void enterAtom(FlaskPythonParser.AtomContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#atom}.
+	 * @param ctx the parse tree
+	 */
+	void exitAtom(FlaskPythonParser.AtomContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#dictLiteral}.
+	 * @param ctx the parse tree
+	 */
+	void enterDictLiteral(FlaskPythonParser.DictLiteralContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#dictLiteral}.
+	 * @param ctx the parse tree
+	 */
+	void exitDictLiteral(FlaskPythonParser.DictLiteralContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#listLiteral}.
+	 * @param ctx the parse tree
+	 */
+	void enterListLiteral(FlaskPythonParser.ListLiteralContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#listLiteral}.
+	 * @param ctx the parse tree
+	 */
+	void exitListLiteral(FlaskPythonParser.ListLiteralContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link FlaskPythonParser#argument}.
+	 * @param ctx the parse tree
+	 */
+	void enterArgument(FlaskPythonParser.ArgumentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link FlaskPythonParser#argument}.
+	 * @param ctx the parse tree
+	 */
+	void exitArgument(FlaskPythonParser.ArgumentContext ctx);
+}
