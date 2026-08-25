bb315f7b86dbeee33ca50658fca2671bca60e9b8
YahyaKabbani <iiyahyakabbani@gmail.com>
2026-08-25 20:40:24 +0300

    +3 semantic errors

--- src/ast/python/DictNode.java
+++ src/ast/python/DictNode.java
@@ -2,6 +2,7 @@ package ast.python;
 
 import ast.ASTNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DictNode extends PythonNode {
@@ -20,6 +21,13 @@ public class DictNode extends PythonNode {
             if (key.equals(e.getKeyText())) return e.getValue();
         }
         return null;
+    }
+
+    @Override
+    public List<String> knownKeys() {
+        List<String> keys = new ArrayList<>();
+        for (DictEntryNode e : entries) keys.add(e.getKeyText());
+        return keys;
     }
 
     @Override
